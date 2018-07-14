9a6a98093f1b603d6aebc4349bb36cbc28377fdc
unknown <kaluzhenov95@gmail.com>
2018-07-14 16:53:21 +0300

    added style for tabs, button and checkbox

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
 $(document).ready( function() {
 
-	// Табы
+	// Tabs
 	$('.wrapper').on('click', '.tab', function() {
-		// Удаление класса .active
+		// Delete a class .active
 		$('.wrapper').find('.active').removeClass('active');
 
-		// Добавляем класс .active
+		// Add a class .active
 		$(this).addClass('active');
 		$('.tab-form').eq( $(this).index() ).addClass('active');
+	});
 
-	})
-})
+	// Floating label
+	$('.input-field input').focus( function() {
+		$(this).parent().addClass('focus');
+	}).blur( function() {
+		if( $(this).val() === '') {
+			$(this).parent().removeClass('focus');
+		}
+	});
+
+});
